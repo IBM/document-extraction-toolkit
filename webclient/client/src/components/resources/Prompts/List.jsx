19fe976f30d6fa127c4d0cc603b280e4d14b6af5
bobfang <7771298+bobfang@users.noreply.github.com>
2024-01-05 14:50:04 -0800

    feat: minor enhancements to prompts and relations display

--- webclient/client/src/components/resources/Prompts/List.jsx
+++ webclient/client/src/components/resources/Prompts/List.jsx
@@ -1,10 +1,34 @@
-import { Datagrid, TextField, ArrayField, SingleFieldList, NumberField,ChipField } from 'react-admin'
-import { Typography } from '@mui/material'
-import {  List, ShowButton, EditButton, RelativeTimeField } from 'components/carbon-ra'
+import { Datagrid, TextField, ArrayField, SingleFieldList, NumberField, ChipField, SimpleShowLayout } from 'react-admin'
+import { useMediaQuery } from '@mui/material'
+import { List, ShowButton, EditButton, RelativeTimeField } from 'components/carbon-ra'
 
-const PromptList = () => (
+const PostShow = () => {
+  return (
+    <SimpleShowLayout>
+      <TextField source="template" />
+      <ArrayField source="questions">
+        <Datagrid bulkActionButtons={false} >
+          <TextField source="field" />
+          <TextField source="question" />
+        </Datagrid>
+      </ArrayField>
+    </SimpleShowLayout>
+  )
+};
+
+const PromptList = () => {
+  const isDesktop = useMediaQuery((theme) => theme.breakpoints.up('md'))
+  return (
   <List hasCreate sort={{ field: 'updated_at', order: 'DESC' }}  >
-    <Datagrid>
+    <Datagrid expand={<PostShow />}
+      sx={{
+        '& td.column-template': {
+          maxWidth: isDesktop ? 400 : 200,
+          overflow: 'hidden',
+          textOverflow: 'ellipsis',
+          whiteSpace: 'nowrap',
+        },
+      }}>
       <TextField source="id" />
       <ShowButton />
       <EditButton />
@@ -13,7 +37,7 @@ const PromptList = () => (
       <NumberField source="model_configuration.min_new_tokens" />
       <NumberField source="model_configuration.max_new_tokens" />
       <NumberField source="model_configuration.temperature" />
-      <TextField source="template"/>
+      <TextField source="template" />
       <ArrayField source="questions">
         <SingleFieldList inline>
           <ChipField source="field" />
@@ -25,6 +49,6 @@ const PromptList = () => (
       <RelativeTimeField source="updated_at" />
     </Datagrid>
   </List>
-)
+)}
 
 export default PromptList
--- webclient/client/src/components/resources/Relations/List.jsx
+++ webclient/client/src/components/resources/Relations/List.jsx
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import { Datagrid, TextField, ReferenceField, DateField, useRecordContext, SimpleShowLayout, BooleanField, ArrayField, SingleFieldList, ChipField } from 'react-admin'
 import { Typography, useMediaQuery } from '@mui/material'
 import { EditButton, List, ShowButton, RelativeTimeField } from 'components/carbon-ra'
@@ -13,77 +13,66 @@ const PostShow = () => {
   useEffect(() => {
     if (!contentLink) {
       console.log(`getting signed link for ${record.s3_key}`)
-      fetch( `/api/s3/getlink?bucket=${record.s3_bucket}&key=${record.s3_key}`, {
+      fetch(`/api/s3/getlink?bucket=${record.s3_bucket}&key=${record.s3_key}`, {
         method: 'GET',
         headers: {
           'Authorization': `Bearer ${access_token}`,
           'Content-Type': 'application/json',
         }
       })
-      .then(response => response.json())
-      .then(data => {
-        setContentLink(data.link)
-      })
-      .catch(error => {
-        console.error('Error fetching pre signed s3 link', error);
-      }); 
+        .then(response => response.json())
+        .then(data => {
+          setContentLink(data.link)
+        })
+        .catch(error => {
+          console.error('Error fetching pre signed s3 link', error);
+        });
     }
   }, [record.s3_key]); // The empty dependency array ensures this effect runs only once
 
   return (
-  <SimpleShowLayout>
-    <ArrayField source="extraction_results">
-      <Datagrid bulkActionButtons={false} >
-        <TextField source="field"/>
-        <TextField source="value" />
-      </Datagrid>
-    </ArrayField>
-    <div>
-      <img src={`${contentLink}`} alt='annotated pdf file' />
-    </div>
-  </SimpleShowLayout>
-)};
+    <SimpleShowLayout>
+      <ArrayField source="extraction_results">
+        <Datagrid bulkActionButtons={false} >
+          <TextField source="field" />
+          <TextField source="value" />
+        </Datagrid>
+      </ArrayField>
+      <div>
+        <img src={`${contentLink}`} alt='annotated pdf file' />
+      </div>
+    </SimpleShowLayout>
+  )
+};
 
 const PostList = () => {
-  const isDesktop = useMediaQuery((theme) => theme.breakpoints.up('md'))
   return (
     <List hasCreate filters={ListFilter}
-    sort={{ field: 'updated_at', order: 'DESC' }}>
-    <Datagrid expand={<PostShow />}
-            sx={{
-              '& td.column-mdi': {
-                maxWidth: isDesktop ? 400 : 200,
-                overflow: 'hidden',
-                textOverflow: 'ellipsis',
-                whiteSpace: 'nowrap',
-              },
-            }}
-            expandSingle
-    >
-      {/* <TextField source="id" /> */}
-      {/* <ShowButton /> */}
-
-      <ReferenceField source="doc_id" reference="documents" link="show" />
-      <TextField source="page_info" />
-      <ReferenceField source="prompt_id" reference="prompts" link="show" />
-      {/* <ReferenceField source="user_id" reference="users" /> */}
-      <ArrayField source="extraction_results">
-        <SingleFieldList inline>
-          <ChipField source="field" />
-          {/* <TextField source="question"/> */}
-        </SingleFieldList>
-      </ArrayField>
-      {/* <TextField source="location_is_record" fullWidth component="pre" /> */}
-      <BooleanField source="needs_review" TrueIcon={ZoomIn} FalseIcon={CertificateCheck} />
-      <EditButton />
-      <TextField source="comments" fullWidth/>
-      {/* <TextField source="index_start" />
+      sort={{ field: 'updated_at', order: 'DESC' }}>
+      <Datagrid expand={<PostShow />}
+        expandSingle
+      >
+        {/* <TextField source="id" /> */}
+        <ReferenceField source="doc_id" reference="documents" link="show" />
+        <TextField source="page_info" />
+        <ReferenceField source="prompt_id" reference="prompts" link="show" />
+        {/* <ReferenceField source="user_id" reference="users" /> */}
+        <ArrayField source="extraction_results">
+          <SingleFieldList inline>
+            <ChipField source="field" />
+            {/* <TextField source="question"/> */}
+          </SingleFieldList>
+        </ArrayField>
+        <BooleanField source="needs_review" TrueIcon={ZoomIn} FalseIcon={CertificateCheck} />
+        <EditButton />
+        <ShowButton />
+        <TextField source="comments" fullWidth />
+        {/* <TextField source="index_start" />
       <TextField source="index_end" /> */}
-      {/* <RelativeTimeField source="created_at" /> */}
-      <RelativeTimeField source="updated_at" />
-
-    </Datagrid>
-  </List>
+        {/* <RelativeTimeField source="created_at" /> */}
+        <RelativeTimeField source="updated_at" />
+      </Datagrid>
+    </List>
   )
 }
 
